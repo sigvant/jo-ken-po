2c076c04ffc2b0198dc096d595a3cf9d9025c0da
sigvant <bnsviv@gmail.com>
2021-07-14 10:18:16 -0300

    final commit

--- script.js
+++ script.js
@@ -25,44 +25,65 @@ function playRound(choice) {
 
     if(p1 == cpu) {
         const span = document.getElementById('results');
-        span.textContent = `CPU played Rock! It's a Draw!`;
+        span.textContent = `CPU played ${choice}! It's a Draw!`;
+        
 
     } else if (p1 == 'Rock' && cpu == 'Scissor') {
         const span = document.getElementById('results');
         span.textContent = `CPU played Scissor! Victory!`;
+        
         p1Score += 1;
 
     } else if (p1 == 'Rock' && cpu == 'Paper') {
         const span = document.getElementById('results');
         span.textContent = `CPU played Paper! Defeat!`;
+        
         cpuScore += 1;
 
     } else if (p1 == 'Paper' && cpu == 'Rock') {
         const span = document.getElementById('results');
         span.textContent = `CPU played Rock! Victory!`;
+        
         p1Score += 1;
 
     } else if (p1 == 'Paper' && cpu == 'Scissor') {
         const span = document.getElementById('results');
         span.textContent = `CPU played Scissor! Defeat!`;
+        
         cpuScore += 1;
 
     } else if (p1 == 'Scissor' && cpu == 'Paper') {
         const span = document.getElementById('results');
         span.textContent = `CPU played Paper! Victory!`;
+        
         p1Score += 1;
 
     } else if (p1 == 'Scissor' && cpu == 'Rock') {
         const span = document.getElementById('results');
         span.textContent = `CPU played Rock! Defeat!`;
+        
         cpuScore += 1;
     }   
-    console.log(`\nPlayer Score:${p1Score}\nCPU Score:${cpuScore}`)
+    
+    const scoreSpan = document.getElementById('score');
+    scoreSpan.textContent = `Player Score: ${p1Score} \nCPU Score: ${cpuScore}`;
+
+    if(p1Score == 3) {
+        const finalScore = document.querySelector('.choices');
+        finalScore.textContent = `You won!`;      
+              
+    } else if(cpuScore == 3) {
+        const finalScore = document.querySelector('.choices');
+        finalScore.textContent = `You Lost!`;
+        
+    }
 }
 
 
 let cpuScore = 0;
 let p1Score = 0;
+let rounds = 3;
+
 
 const iChooseRock = document.getElementById('rock');
 iChooseRock.addEventListener('click', () => {playRound('Rock')});
@@ -72,3 +93,5 @@ iChoosePaper.addEventListener('click', () => {playRound('Paper')});
 
 const iChooseScissor = document.getElementById('scissor');
 iChooseScissor.addEventListener('click', () => {playRound('Scissor')});
+
+
